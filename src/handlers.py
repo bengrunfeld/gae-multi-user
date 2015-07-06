b66da53577f20311614db1fe0f42fdb5e5db2ec6
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-07-05 22:59:33 -0700

    add Google Account login

--- src/handlers.py
+++ src/handlers.py
@@ -7,77 +7,127 @@ They execute commands that retrieve, store, update and delete resources.
 import json
 import webapp2
 import datetime
+import jinja2
+import os
 
 from google.appengine.ext import ndb
+from google.appengine.api import users
+from webapp2_extras import sessions
 
-from models import TodoModel
+TEMPLATE_DIR = 'templates'
+TEMPLATE_SUFFIX = '.html'
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'that-looks-uncomfortably-large',
+}
+
+class TodoModel(ndb.Model):
+    "Models an individual todo title"
+    title = ndb.StringProperty()
+    time_stored = ndb.DateTimeProperty(auto_now_add=True)
 
 
-def build_new_dict(data):
-    """Build a new dict so that the data can be JSON serializable"""
+class BaseHandler(webapp2.RequestHandler):
+    """
+    Set up sessions and perform template renders
+    """
 
-    result = data.to_dict()
-    record = {}
+    def dispatch(self):
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
 
-    # Populate the new dict with JSON serializiable values
-    for key in result.iterkeys():
-        if isinstance(result[key], datetime.datetime):
-            record[key] = result[key].isoformat()
-            continue
-        record[key] = result[key]
-    
-    # Add the key so that we have a reference to the record
-    record['key'] = data.key.id()
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
 
-    return record
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
+
+class GoToLoginPage(webapp2.RequestHandler):
+    """
+    1. Load the login page 
+    """
+
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('login.html')
+        self.response.write(template.render())
+
+
+class SignIn(BaseHandler):
+    """
+    2. Log the User in using Google Accounts 
+    """
+
+    def get(self):
+        if self.session.get('logged_in'):
+            # User is logged in, proceed to account
+            self.redirect('/account') 
+            return
+
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        if user:
+            # Redirect to Accounts page
+            self.session['logged_in'] = True
+            self.redirect('/account')
+        else:
+            self.session['logged_in'] = False
+            self.redirect(users.create_login_url(self.request.uri))
+
+
+class LoadAccount(BaseHandler):
+    """
+    3. Render the front end of the app, with User data
+    """
+
+    def get(self):
+        # Check if User is logged in. If not, send to auth
+
+        if not self.session.get('logged_in'):
+            self.redirect('/sign-in')
+            return
+
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        print user.user_id()
+
+        template_data = {
+            'username': user.nickname(),
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('account.html')
+        self.response.write(template.render(template_data))
+
+
+class Logout(BaseHandler):
+    """
+    4. Logout from app and reset user login session 
+    """
+
+    def get(self):
+        self.session['logged_in'] = False
+        self.redirect(users.create_logout_url('/'))
+        return
         
 
-def serialize_data(qry):
-    """serialize ndb return data so that we can convert it to JSON"""
-    
-    # check if qry is a list (multiple records) or not (single record)
-    data = []
-    
-    if type(qry) != list: 
-        record = build_new_dict(qry) 
-        return record
-
-    for q in qry:
-        data.append(build_new_dict(q))
-
-    return data 
-
-
-class Login(webapp2.RequestHandler):
-    """
-    Check that the user is logged in, then render the front end of the app
-    """
-
-    def get(self):
-
-        print '---------------'
-        print 'here'
-        print '---------------'
-
-        # Check that user is logged in. Send to auth if False
-        # if not self.session.get('logged_in'):
-        #     self.redirect('/auth')
-        #     return
-
-        # Get data for render
-        # data = _get_front_page_data()
-
-        # Put data into context
-        # context = {"repos": data, "username": get_user_name()}
-
-        # Render the app
-        # self.render('index', context)
-
-
 class GetAllTodos(webapp2.RequestHandler):
-    def get(self):
-        """GET /: Retrieve all todos"""
+    """GET /: Retrieve all todos"""
 
+    def get(self):
         try:
             qry = TodoModel.query().fetch()
             all_todos = serialize_data(qry)
@@ -89,24 +139,10 @@ class GetAllTodos(webapp2.RequestHandler):
             raise Exception("Error: could not complete request")            
 
 
-class GetTodo(webapp2.RequestHandler):
-    def get(self, todo_id):
-            """GET /<todo_id>: Retrieve a single todo"""
-
-        try:
-            qry = ndb.Key('TodoModel', int(todo_id))
-            record = serialize_data(qry.get())
-            
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write(json.dumps(record, sort_keys=True, indent=4))
-        except:
-            raise Exception("Error: could not complete request")
-
-
 class CreateTodo(webapp2.RequestHandler):
-    def post(self):
-        """POST /: Create a single todo"""
+    """POST /: Create a single todo"""
 
+    def post(self):
         try:
             new_todo = TodoModel(title = self.request.get('title')) 
             key = new_todo.put()
@@ -116,32 +152,9 @@ class CreateTodo(webapp2.RequestHandler):
         except:
             raise Exception("Error: could not complete request")
 
-
-class UpdateTodo(webapp2.RequestHandler):
-    def put(self, todo_id):
-        """PUT /<todo_id>: Update a single todo""" 
-        
-        try:
-            qry = ndb.Key('TodoModel', int(todo_id))
-            target = qry.get()
-            target.title = self.request.get('title')
-            target.put()
-            
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('Record was updated')
-        except:
-            raise Exception("Error: could not complete request")
-
-
-class DeleteTodo(webapp2.RequestHandler):
-    def delete(self, todo_id):
-        """DELETE /<todo_id>: Delete a single todo"""
-
-        try:
-            qry = ndb.Key('TodoModel', int(todo_id))
-            qry.delete()
-
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('{} was deleted'.format(todo_id))
-        except:
-            raise Exception("Error: could not complete request")
+app = webapp2.WSGIApplication([
+   ('/sign-in', SignIn),
+   ('/account', LoadAccount),
+   ('/logout', Logout),
+   ('/', GoToLoginPage),
+], config=config)
